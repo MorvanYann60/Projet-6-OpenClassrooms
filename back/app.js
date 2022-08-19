1b1c3489db00b6786c5fe423b705000eb38f8856
Yann Morvan <yancyn60500@hotmail.fr>
2022-08-19 10:42:44 +0200

    Projet 6 OpenClassrooms

---
+++ back/app.js
@@ -0,0 +1,40 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+mongoose.connect('mongodb+srv://Yann:Louna60500@cluster0.mwfouqm.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(express.json());
+
+// app.post('/api/auth/login', (req, res, next) => {
+//     console.log(req.body);
+//     res.status(201).json({
+//         message: "Objet passé !"
+//     })
+// });
+
+// app.post('/api/auth/signup', (req, res, next) => {
+//   console.log(req.body);
+//   res.status(201).json({
+//     message: "Objet2 passé !"
+//   })
+// });
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
