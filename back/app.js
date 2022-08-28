4b23ab2ee85c95581f4a386b8f5bbebd310569da
Yann Morvan <yancyn60500@hotmail.fr>
2022-08-29 00:33:34 +0200

    modification

--- back/app.js
+++ back/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+require("dotenv").config();
 
 const userRoutes = require("./routes/user");
 const saucesRoutes = require("./routes/sauces");
